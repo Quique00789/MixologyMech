199ddb6b80c00effb0a4fb360bb6426f0b09d16e
Ianrojo14 <144838820+Ianrojo14@users.noreply.github.com>
2024-06-18 10:04:45 -0600

    Comandos instalacion firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDYpo48YX6YW9c2SPi5-KwFAtISTK91KFc",
+  authDomain: "mixologymech.firebaseapp.com",
+  projectId: "mixologymech",
+  storageBucket: "mixologymech.appspot.com",
+  messagingSenderId: "202161090929",
+  appId: "1:202161090929:web:ce56fea156886114bb06d0",
+  measurementId: "G-CQYC412V3V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
